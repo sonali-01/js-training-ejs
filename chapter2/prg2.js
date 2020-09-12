56e9c1a942ec1a488cf3731fb31a7a3bb30d71dd
sonali-01 <sonaliskumar98@gmail.com>
2020-09-12 15:37:49 +0530

    2nd exercise

---
+++ chapter2/prg2.js
@@ -0,0 +1,19 @@
+let a = [];
+function range(s, e) {
+  for (let i = s; i <= e; i++) {
+    a.push(i);
+  }
+  return a;
+}
+let number = range(1, 100);
+for (let i = 0; i < number.length; i++) {
+  if (number[i] % 5 == 0 && number[i] % 3 == 0) {
+    console.log("FizzBuzz");
+  } else if (number[i] % 3 == 0) {
+    console.log("Fizz");
+  } else if (number[i] % 5 == 0 && number[i] % 3 !== 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number[i]);
+  }
+}
